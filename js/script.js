bbfae8d8d5b28179fc0cba760fefaf4f7b760d64
blessMe-Padre <zarodiny@yandex.ru>
2021-06-25 18:43:03 +1000

    Внесение исправлений

--- js/script.js
+++ js/script.js
@@ -1,127 +1,67 @@
-const popupLinks = document.querySelectorAll('.popup-link');
-const body = document.querySelector('body');
-const lockPadding = document.querySelectorAll(".lock-padding");
+const openPopup = document.querySelector(".popup-link");
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".close-popup");
+const modal = document.querySelector(".popup__body");
+const popupForm = popup.querySelector(".modal-form");
+const loginName = popup.querySelector(".user-name");
+const loginEmail = popup.querySelector(".user-email");
 
-let unlock = true;
 
-const timeout = 800;
+let isStorageSupport = true;
+let storage = "";
 
-if (popupLinks.length > 0) {
-  for (let index = 0; index < popupLinks.length; index++) {
-    const popupLink = popupLinks[index];
-    popupLink.addEventListener("click", function (e) {
-      const popupName = popupLink.getAttribute('href').replace('#', '');
-      const curentPopup = document.getElementById(popupName);
-      popupOpen(curentPopup);
-      e.preventDefault();
-    });
-  }
-}
-const popupCloseIcon = document.querySelectorAll('.close-popup');
-if (popupCloseIcon.length > 0) {
-  for (let index = 0; index < popupCloseIcon.length; index++) {
-    const el = popupCloseIcon[index];
-    el.addEventListener('click', function (e) {
-      popupClose(el.closest('.popup'));
-      e.preventDefault();
-    });
-  }
+try {
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
 }
 
-function popupOpen(curentPopup) {
-  if (curentPopup && unlock) {
-    const popupActive = document.querySelector('.popup.open');
-    if (popupActive) {
-      popupClose(popupActive, false);
-    } else {
-      bodyLock();
-    }
-    curentPopup.classList.add('open');
-    curentPopup.addEventListener("click", function (e) {
-      if (!e.target.closest('.popup__content')) {
-        popupClose(e.target.closest('.popup'));
-      }
-    });
-  }
-}
+openPopup.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("popup-show");
 
-function popupClose(popupActive, doUnlock = true) {
-  if (unlock) {
-    popupActive.classList.remove('open');
-    if (doUnlock) {
-      bodyUnLock();
-    }
-  }
-}
-
-function bodyLock() {
-  const lockPaddingValue = window.innerWidth - document.querySelector('.page-body').offsetWidth + 'px';
-
-  if (lockPadding.length > 0) {
-    for (let index = 0; index < lockPadding.length; index++) {
-      const el = lockPadding[index];
-      el.style.paddingRight = lockPaddingValue;
-    }
-  }
-  body.style.paddingRight = lockPaddingValue;
-  body.classList.add('lock');
-
-  unlock = false;
-  setTimeout(function () {
-    unlock = true;
-  }, timeout);
-}
-
-function bodyUnLock() {
-  setTimeout(function () {
-    if (lockPadding.length > 0) {
-      for (let index = 0; index < lockPadding.length; index++) {
-        const el = lockPadding[index];
-        el.style.paddingRight = '0px';
-      }
-    }
-    body.style.paddingRight = '0px';
-    body.classList.remove('lock');
-  }, timeout);
-
-  unlock = false;
-  setTimeout(function () {
-    unlock = true;
-  }, timeout);
-}
-
-document.addEventListener('keydown', function (e) {
-  if (e.which === 27) {
-    const popupActive = document.querySelector('.popup.open');
-    popupClose(popupActive);
+  if (storage) {
+    loginName.value = storage;
+    loginEmail.focus();
+  } else {
+    loginName.focus();
   }
 });
 
-(function () {
-  // проверяем поддержку
-  if (!Element.prototype.closest) {
-    // реализуем
-    Element.prototype.closest = function (css) {
-      var node = this;
-      while (node) {
-        if (node.matches(css)) return node;
-        else node = node.parentElement;
-      }
-      return null;
-    };
-  }
-})();
-(function () {
-  // проверяем поддержку
-  if (!Element.prototype.matches) {
-    // определяем свойство
-    Element.prototype.matches = Element.prototype.matchesSelector ||
-      Element.prototype.webkitMatchesSelector ||
-      Element.prototype.mozMatchesSelector ||
-      Element.prototype.msMatchesSelector;
-  }
-})();
+closePopup.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("popup-show");
+  popup.classList.remove("popup-error");
+});
 
+popupForm.addEventListener("submit", function (evt) {
+  if (!loginName.value || !loginEmail.value) {
+    evt.preventDefault();
+    popup.classList.remove("popup-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("popup-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", loginName.value);
+    }
+  }
+});
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    popup.classList.remove("popup-show");
+  }
+};
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.key === "Esc" || evt.key === "Escape") {
+    if (popup.classList.contains("popup-show")) {
+      evt.preventDefault();
+      popup.classList.remove("popup-show");
+      popup.classList.remove("popup-error");
+    }
+  }
+});
 
 ymaps.ready(function () {
   var myMap = new ymaps.Map('map', {
@@ -130,50 +70,17 @@ ymaps.ready(function () {
   }, {
     searchControlProvider: 'yandex#search'
   }),
-
-    // Создаём макет содержимого.
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    ),
-
     myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
       hintContent: 'nёrds design studio',
       balloonContent: 'наб. Реки Карповки, 5 лит П, Санкт-Петербург'
     }, {
-      // Опции.
-      // Необходимо указать данный тип макета.
       iconLayout: 'default#image',
-      // Своё изображение иконки метки.
       iconImageHref: 'img/map-marker.png',
-      // Размеры метки.
       iconImageSize: [231, 190],
-      // Смещение левого верхнего угла иконки относительно
-      // её "ножки" (точки привязки).
       iconImageOffset: [-50, -190]
-    }),
-
-    myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
-      hintContent: 'Собственный значок метки с контентом',
-      balloonContent: 'А эта — новогодняя',
-      iconContent: '12'
-    }, {
-      // Опции.
-      // Необходимо указать данный тип макета.
-      iconLayout: 'default#imageWithContent',
-      // Своё изображение иконки метки.
-      iconImageHref: 'images/ball.png',
-      // Размеры метки.
-      iconImageSize: [48, 48],
-      // Смещение левого верхнего угла иконки относительно
-      // её "ножки" (точки привязки).
-      iconImageOffset: [-24, -24],
-      // Смещение слоя с содержимым относительно слоя с картинкой.
-      iconContentOffset: [15, 15],
-      // Макет содержимого.
-      iconContentLayout: MyIconContentLayout
     });
-
+  myMap.behaviors.disable('scrollZoom');
   myMap.geoObjects
-    .add(myPlacemark)
-    .add(myPlacemarkWithContent);
+    .add(myPlacemark);
 });
+
